0808590a09ca71dfabd6a589d60308dc1be794d0
Nzola Kiampava <nzolakiampava@gmail.com>
2022-10-08 19:19:03 -0300

    Update restaurants.controller.js

--- api/restaurants.controller.js
+++ api/restaurants.controller.js
@@ -1,32 +1,57 @@
-import RestaurantsDAO from '../dao/restaurantsDAO.js'
+import RestaurantsDAO from "../dao/restaurantsDAO.js";
 
 export default class RestaurantsController {
     static async apiGetRestaurants(req, res, next) {
         const restaurantsPerPage = req.query.restaurantsPerPage ? parseInt(req.query.restaurantsPerPage, 10) : 20
         const page = req.query.page ? parseInt(req.query.page, 10) : 0
-    
+
         let filters = {}
+
         if (req.query.cuisine) {
-          filters.cuisine = req.query.cuisine
+            filters.cuisine = req.query.cuisine
         } else if (req.query.zipcode) {
-          filters.zipcode = req.query.zipcode
+            filters.zipcode = req.query.zipcode
         } else if (req.query.name) {
-          filters.name = req.query.name
+            filters.name = req.query.name
         }
-    
+
         const { restaurantsList, totalNumRestaurants } = await RestaurantsDAO.getRestaurants({
-          filters,
-          page,
-          restaurantsPerPage,
+            filters,
+            page,
+            restaurantsPerPage,
         })
-    
+
         let response = {
-          restaurants: restaurantsList,
-          page: page,
-          filters: filters,
-          entries_per_page: restaurantsPerPage,
-          total_results: totalNumRestaurants,
+            restaurants: restaurantsList,
+            page: page,
+            filters: filters,
+            entries_per_page: restaurantsPerPage,
         }
         res.json(response)
+    }
+
+    static async apiGetRestaurantById(req, res, next) {
+        try {
+            let id = req.params.id || {}  // params means, get id after slash(/)
+            let restaurant = await RestaurantsDAO.getRestaurantByID(id)
+            if (!restaurant) {            // if thereisn't restaurants
+                res.status(404).json({ error: "NOT FOUND" })
+                return
+            } 
+            res.json(restaurant)
+        } catch (e) {
+            console.log(`api, ${e}`)
+            res.status(500).json({ error: e })
+        }
+    }
+
+    static async apiGetRestaurantCuisines(req, res, next) {
+        try {
+            let cuisine = await RestaurantsDAO.getCuisines()
+            res.json(cuisine)
+        } catch (e) {
+            console.log(`api, ${e}`)
+            res.status(500).json({ error: e })
+        }
     }
 }
